3fa26cdd6b0006e58f46bdf5386081f07e9eab5c
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-10-13 16:42:57 +0300

    refactor: temporary delete persistor v2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
 import App from 'components/App';
 import './index.css';
 
